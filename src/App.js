1c31432da4a866eb5556a9474de362f67aa5578a
Marcin Bołba <marcin@MarcinPc.dipper.com>
2018-08-13 22:56:06 +0200

    Add courses tab

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
-import Calendar from './components/home/Calendar'
-import NavigationBar from './components/navigationBar/NavigationBar'
+import Navigation from './components/navigation/Navigation'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <div >
-            <NavigationBar/>
-            <Switch>
-              <Route path="/" component={Calendar} exact />
-            </Switch>
-          </div>
-        </BrowserRouter>
+        <Navigation />
       </div>
     );
   }
