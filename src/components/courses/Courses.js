e8761a0bfcbdc63f339d9e9418076174f12a304e
Marcin Bołba <marcin@MarcinPc.dipper.com>
2018-09-07 15:16:43 +0200

    Add courses input form

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -1,17 +1,90 @@
 import React, { Component } from 'react';
 import './Courses.css'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 
 class Courses extends Component {
     state = {
-        NewCourseName:  "",
-        NewTerm: null,
+        NewCourseName: "",
+        NewCourseType: "Typ",
+        NewCouseDay: "Dzień",
+        NewCouseStartTime: null,
+        NewCouseEndTime: null,
         StartDate: null,
         EndDate: null
     }
     onSubmitHandler = () => {
-        console.log(this.state.NewCourseName)
-        console.log(this.state.NewTerm)
+        if (this.state.NewCourseName !== "" && this.state.NewCourseType !== "Typ" && this.state.NewCouseDay !== "Dzień" && this.state.NewCouseStartTime && this.state.NewCouseEndTime &&this.state.StartDate && this.state.EndDate)
+            console.log("ok")
+        else
+            console.log("err")
+    }
+    inputFrom = () => {
+        return (
+            <tr>
+                <td><input type="text" onChange={(e) => { this.setState({ NewCourseName: e.target.value }) }} value={this.state.NewCourseName} /></td>
+                <td>
+                    <div className="dropdown">
+                        <div>{this.state.NewCouseDay}</div>
+                        <div className="dropdown-content">
+                            <a onClick={() => { this.setState({ NewCouseDay: "Pon" }) }}    >Poniedziałek</a>
+                            <a onClick={() => { this.setState({ NewCouseDay: "Wt" }) }}     >Wtorek</a>
+                            <a onClick={() => { this.setState({ NewCouseDay: "Śr" }) }}     >Środa</a>
+                            <a onClick={() => { this.setState({ NewCouseDay: "Czw" }) }}    >Czwartek</a>
+                            <a onClick={() => { this.setState({ NewCouseDay: "Pt" }) }}     >Piątek</a>
+                            <a onClick={() => { this.setState({ NewCouseDay: "Sob" }) }}    >Sobota</a>
+                            <a onClick={() => { this.setState({ NewCouseDay: "Nd" }) }}     >Niedziela</a>
+                        </div>
+                    </div>
+                </td>
+                <td><input type="time" id="appt-time" name="appt-time" onChange={(e)=>{
+                    this.setState({
+                        NewCouseStartTime: e.target.value
+                    })
+                }}/></td>
+                <td><input type="time" id="appt-time" name="appt-time" onChange={(e)=>{
+                    this.setState({
+                        NewCouseEndTime: e.target.value
+                    })
+                }}/></td>
+                <td>
+                    <div className="dropdown">
+                        <div>{this.state.NewCourseType}</div>
+                        <div className="dropdown-content">
+                            <a onClick={() => { this.setState({ NewCourseType: "Wykład" }) }}       >Wykład</a>
+                            <a onClick={() => { this.setState({ NewCourseType: "Laboratoria" }) }}  >Laboratoria</a>
+                            <a onClick={() => { this.setState({ NewCourseType: "Zajęcia" }) }}      >Zajęcia</a>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <DatePicker
+                        selected={this.state.StartDate}
+                        onChange={(date) => {
+                            this.setState({
+                                StartDate: date
+                            })
+                        }}
+                        isClearable={true}
+                        placeholderText="Data początku kursu"
+                    />
+                </td>
+                <td>
+                    <DatePicker
+                        selected={this.state.EndDate}
+                        onChange={(date) => {
+                            this.setState({
+                                EndDate: date
+                            })
+                        }}
+                        isClearable={true}
+                        placeholderText="Data końca kursu"
+                    />
+                </td>
+                <td><input type="submit" onClick={this.onSubmitHandler} /></td>
+            </tr>
+        )
     }
     render() {
         var today = new Date()
@@ -21,34 +94,15 @@ class Courses extends Component {
                     <thead className="TableHead">
                         <tr>
                             <th>Przedniot</th>
-                            <th>Termin[]</th>
+                            <th colSpan="3">Dzień i godzina zajęć</th>
+                            <th>Typ Zajęć</th>
                             <th>Od</th>
                             <th>Do</th>
-                            <th>Operacja</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Matematyka stosowana</td>
-                            <td>{today.toUTCString()}</td>
-                            <td>{today.toUTCString()}</td>
-                            <td>{today.toUTCString()}</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Fizyka</td>
-                            <td>{today.toUTCString()}</td>
-                            <td>{today.toUTCString()}</td>
-                            <td>{today.toUTCString()}</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td><input type="text" onChange={(e)=>{this.setState({NewCourseName:e.target.value})}} value={this.state.NewCourseName}/></td>
-                            <td><input type="date" onChange={(e)=>{this.setState({NewTerm:e.target.value})}} /></td>
-                            <td><input type="date"/></td>
-                            <td><input type="date"/></td>
-                            <td><input type="submit" onClick={this.onSubmitHandler}/></td>
-                        </tr>
+                        {this.inputFrom()}
                     </tbody>
                 </table>
             </div>
